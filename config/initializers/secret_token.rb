a03b4bcd8e5e9933d05fbdf0158988b63cdb0e44
indivisible-irl <mbspare@gmail.com>
2012-12-08 13:08:00 +0000

    set up rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tut02::Application.config.secret_token = '54d3dba53f8e82c04330fdaf87c10f2176b5257a0dc0a09f74148b852c91caaa7070bdcc0866f77918782d85d754a524f1127539ca4631ca02393eaedde08f86'
