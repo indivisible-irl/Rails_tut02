b77a7a1ad3cab8b37666038150539df96ded55fb
indivisible-irl <mbspare@gmail.com>
2012-12-11 12:41:20 +0000

    help page & title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	# return a title on a per-page basis
+	def title
+		base_title = "RubyOnRails Tut02"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+
+	
 end
